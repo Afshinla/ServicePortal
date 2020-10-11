9b41a0fc6dee9d2c63b557d0f8260e91f9bd09ca
Afshinla <afshinla@gmail.com>
2020-10-11 18:08:00 +0200

    Incident.HTML

---
+++ script/incident.js
@@ -0,0 +1,26 @@
+function create()
+{
+	//var requestBody = "{\"short_description\":\"Tetser igen\",\"assignment_group\":\"Network\"}"; 
+	var incgroup = document.getElementById ("group").value;
+	var inctitle = document.getElementById ("title").value;
+	
+	var requestBody = "{\"short_description\":\"" + inctitle + "\",\"assignment_group\":\"" + incgroup + "\"}"
+	var client=new XMLHttpRequest();
+	client.open("post","https://dev62205.service-now.com/api/now/table/incident");
+	
+	client.setRequestHeader('Accept','application/json');
+	client.setRequestHeader('Content-Type','application/json');
+	
+	//Eg. UserName="admin", Password="admin" for this code sample.
+	client.setRequestHeader('Authorization', 'Basic '+btoa('admin_afla'+':'+'TanDan00'));
+	
+	client.onreadystatechange = function() { 
+		if(this.readyState == this.DONE) {
+//document.getElementById("response").innerHTML=this.status + this.response;
+			var res = this.response;
+			parseData = JSON.parse(res);
+			alert("Du har oprettet en incident med nummer: " + parseData.result.number);
+		}
+	}; 
+	client.send(requestBody);
+}
